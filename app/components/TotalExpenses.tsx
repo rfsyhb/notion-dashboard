d3fc5fc3878d7863343e2410c07131ff9ae7a6f3
Rafi Syihab <rafisyihab3@gmail.com>
2026-09-23 15:09:39 +0700

    display total expenses and monthly expenses

---
+++ app/components/TotalExpenses.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { getExpenses } from '@/lib/expenses';
+
+export function TotalExpenses() {
+  const expensesQuery = useQuery({
+    queryKey: ['expenses'],
+    queryFn: getExpenses,
+  });
+
+  if (expensesQuery.isPending) {
+    return <p>Loading...</p>;
+  }
+
+  if (expensesQuery.isError) {
+    return <p>{expensesQuery.error.message}</p>;
+  }
+
+  const expenseData = expensesQuery.data.data;
+
+  const now = new Date();
+  // ex "2026-09"
+  const currentMonth = `${now.getFullYear()}-${String(
+    now.getMonth() + 1,
+  ).padStart(2, '0')}`;
+  console.log(currentMonth);
+  console.log(expensesQuery.data.data);
+  const currentMonthExpensesData = expensesQuery.data.data.filter((v) =>
+    v.Date.startsWith(currentMonth),
+  );
+  console.log(currentMonthExpensesData);
+
+  const totalExpenses =
+    expenseData.reduce((total, expense) => total + expense.IDR, 0) ?? 0;
+  const totalExpensesThisMonth =
+    currentMonthExpensesData.reduce(
+      (total, expense) => total + expense.IDR,
+      0,
+    ) ?? 0;
+
+  return (
+    <div className="flex flex-col gap-2 p-2 text-right">
+      <p className="">Total Rp{totalExpenses.toLocaleString('id-ID')}</p>
+      <p className="  ">
+        Bulan ini Rp{totalExpensesThisMonth.toLocaleString('id-ID')}
+      </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image';
+import { TotalExpenses } from './components/TotalExpenses';
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-mono bg-background">
-      <main className='flex flex-row gap-2 items-center'>
-        <Image
-          width={40}
-          height={40}
-          src={'/Notion_app_logo.png'}
-          alt="notion_icon"
-        />
-        <h2 className='text-lg font-semibold'>Rafi&rsquo;s Dashboard</h2>
+      <main className="flex flex-col gap-2 items-center">
+        <div className="flex flex-roww items-center gap-2">
+          <Image
+            width={40}
+            height={40}
+            src={'/Notion_app_logo.png'}
+            alt="notion_icon"
+          />
+          <h2 className="text-lg font-semibold">Rafi&rsquo;s Dashboard</h2>
+        </div>
+        <span className='flex flex-row items-center gap-3'>
+          total spending <TotalExpenses />
+        </span>
       </main>
     </div>
   );
