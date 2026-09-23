a9938869f0b92c22b2ef1a4cfdf3410678c6a291
Rafi Syihab <rafisyihab3@gmail.com>
2026-09-23 16:05:55 +0700

    update input date

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -81,7 +81,7 @@ export default function TestPage() {
 
   return (
     <main className="h-screen p-8 items-center justify-center flex">
-      <form onSubmit={handleSubmit} className="space-y-4">
+      <form onSubmit={handleSubmit} className="space-y-4 max-w-[60%]">
         <div>
           <label className="mb-1 block">Product</label>
 
